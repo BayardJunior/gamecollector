1bcb98270c539edf3f63d232181c2b0aaca37a80
Bayard Junior <sobreira.b@gmail.com>
2017-02-03 16:41:53 -0300

    Agora o app trabalha com Core Data

--- GameCollector/GameViewController.swift
+++ GameCollector/GameViewController.swift
@@ -24,12 +24,39 @@ class GameViewController: UIViewController,UIImagePickerControllerDelegate,UINav
     
     
     @IBAction func addNaTabela(_ sender: Any) {
+        //pega o contexto
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        //cria a entidade
+        let game = Game(context: context)
+        
+        //atribuiçao
+        game.text = descricao.text
+        game.image = UIImagePNGRepresentation(image.image!) as NSData!
+        
+        //salva o contexto
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        //tras a tela anterior
+        navigationController!.popViewController(animated: true)
     }
     
     @IBAction func galeria(_ sender: Any) {
         imagePicker.sourceType = .photoLibrary
-        
+        //apresenta uma nova ViewController
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        //img => o que eu escolhi do grid de imagens
+        let img = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        //atribui a imagem para o outlet de imagem da tela
+        image.image = img
+        
+        //dispensa uma ViewController
+        imagePicker.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func camera(_ sender: Any) {
--- GameCollector/ViewController.swift
+++ GameCollector/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var games : [Game] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //pega o contexto
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            //retorna tudo o que for game do coredata
+            games = try context.fetch(Game.fetchRequest())
+            //atualiza a tabela
+            tableView.reloadData()
+        }catch {
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //retorna a quantidade de itens para a tabela
+        return games.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //cria uma celula
+        let cell = UITableViewCell()
+        //atribui o que tiver detro de games[x] para a celula
+        cell.textLabel?.text = games[indexPath.row].text!
+        
+        //atribui o que tiver detro de games[x] para a celula
+        //conversao de NSData para UIImage
+        cell.imageView?.image = UIImage(data: games[indexPath.row].image! as Data)
+        
+        //retona a celula com as informaçeos
+        return cell
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
